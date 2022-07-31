f412515bb73b3064290f69f43653e5c34de20f14
Ubuntu <ubuntu@ip-172-31-92-5.ec2.internal>
2022-07-31 21:16:29 +0000

    ajustes de configuracoes e adicao do schema registry

--- images/spark-stream/files/start-submit.sh
+++ images/spark-stream/files/start-submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/opt/spark-3.2.1-bin-hadoop2.7/bin/spark-submit \
+/opt/spark-3.2.2-bin-hadoop2.7/bin/spark-submit \
 --master local[*] \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,org.elasticsearch:elasticsearch-spark-30_2.12:7.12.0  \
 --archives /pyspark_venv.tar.gz#environment \
